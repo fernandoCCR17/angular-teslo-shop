e815c47a9e399c67cfee8704978d7a7ddffb2d81
Fernando de la Cruz Ramos <fernan654happy@gmail.com>
2026-03-23 22:18:49 -0600

    feat: add user and product interfaces, implement products service, and update home page to fetch products

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
-import { FrontProductCard } from "../../components/front-product-card/front-product-card";
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@products/services/products.service';
+import { FrontProductCard } from '@store-front/components/front-product-card/front-product-card';
 
 @Component({
   selector: 'app-home-page',
   imports: [FrontProductCard],
   templateUrl: './home-page.html',
 })
-export class HomePage { }
+export class HomePage {
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => {
+      return this.productsService.getProducts();
+    }
+  })
+}
